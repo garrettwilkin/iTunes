d72faa63f6e0bac8efdbe5bb7d64f10832e06a38
garrettwilkin <garrett.wilkin@gmail.com>
2011-01-13 14:11:58 -0500

    MetaMedia is looking much more like an abstracted class of which iTunes will be just one component. Next step is to make MetaMedia and iTunes modules evented.  The iTunes request uses the evented http object, but unless the getArtist call can be used asynchronously, I think that this MetaData module would not be taking advantage of Nodes asynchronicity.

--- itunes.js
+++ itunes.js
@@ -8,6 +8,8 @@
  */
 
 var http = require('http');
+var iResults= require('./iresults').iResults;
+var Timer = require('./timer').Timer;
 var querystring = require('querystring');
 
 /*
@@ -42,12 +44,47 @@ function iTunes() {
     this.params = new iParameters();
     this.basePath = '/WebObjects/MZStoreServices.woa/wa/wsSearch?';
     this.server= 'ax.itunes.apple.com';
+    this.iresults = new iResults();
 };
 exports.iTunes = iTunes;
+
+iTunes.prototype.getArtist = function(artist) {
+   this.params.term = artist.replace(/ /g,'+');
+   this.params.entity = 'musicArtist';
+   this.params.attribute= 'artistTerm';
+   this.params.media = 'music';
+   this.request('itunes',this.params.term)
+};
+
 
 iTunes.prototype.getQuery = function() {
     console.log(this.params);
     var query = querystring.stringify(this.params);
     console.log('QUERY : ' + query);
     return query;
+};
+
+iTunes.prototype.request = function(source, label) {
+    var self = this;
+    var clock = new Timer(label);
+    var apple = http.createClient(80,self.server);
+    var query = self.getQuery();
+    var path = self.basePath + query;
+    console.log('SERVER: ' + self.server);
+    console.log('PATH: ' + path);
+    var request = apple.request('GET',path,{host:self.server});
+    apple.request('GET',path);
+    request.end();
+    clock.set();
+    request.on('response', function(response) {
+        response.setEncoding('utf8');
+        response.on('data', function(chunk) {
+            clock.elapsed();
+            self.iresults.capture(chunk);
+        });
+        response.on('end', function() {
+            clock.elapsed();
+            self.iresults.parse();
+        });
+    });
 };
