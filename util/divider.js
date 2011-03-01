cef5e79c8a2f2402aacc9bd0c3d07de5dd0ad201
Garrett Wilkin <garrett.wilkin@gmail.com>
2011-02-28 22:43:15 -0500

    Removing logging statements from all files except demos.

--- util/divider.js
+++ util/divider.js
@@ -1,12 +1,8 @@
 /*
  * 
  * Abstraction layer for logging.
- * My own wrapper class so that I can change logging packages if need be without changing all my debugging statements.
- * Currently using winston logging package.
  */
 require.paths.unshift(require('path').join(__dirname));
-
-var winston = require('winston');
 
 function Divider(label) {
    this.label = label;
@@ -15,5 +11,5 @@ exports.Divider = Divider;
 
 Divider.prototype.print = function (message) {
     var phrase = this.label + ' : ' + message + '\n';
-    winston.info(phrase);
+    console.log(phrase);
 };
